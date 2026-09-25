50ad1d1828d45c4a1fc39d9415e915d0d7136b83
Ben0p <31563479+Ben0p@users.noreply.github.com>
2026-09-25 08:27:46 +0800

    style: format failed-turn fence test

--- elixir/test/symphony_elixir/orchestrator_execution_fence_test.exs
+++ elixir/test/symphony_elixir/orchestrator_execution_fence_test.exs
@@ -68,12 +68,15 @@ defmodule SymphonyElixir.OrchestratorExecutionFenceTest do
       managed_delegations: %{repository_ref: repository}
     }
 
-    :sys.replace_state(pid, &%{
-      &1
-      | execution_fence: fence,
-        running: %{issue_id => entry},
-        work_package_runtime: runtime
-    })
+    :sys.replace_state(
+      pid,
+      &%{
+        &1
+        | execution_fence: fence,
+          running: %{issue_id => entry},
+          work_package_runtime: runtime
+      }
+    )
 
     update = %{
       event: :turn_failed,
@@ -115,8 +118,10 @@ defmodule SymphonyElixir.OrchestratorExecutionFenceTest do
 
     {:ok, released_fence, :released} = ExecutionFence.release(fence, token, session.session_id)
     :sys.replace_state(pid, &%{&1 | execution_fence: released_fence})
+
     assert {:error, :managed_failed_turn_identity_mismatch} =
              GenServer.call(pid, {:managed_failed_turn, issue_id, update})
+
     assert File.read!(journal_path) == bytes
   end
 
